a630daff5e3af39582a00286254ad4401b0754f1
sudarakas <sudharakafb@gmail.com>
2020-01-09 22:10:25 +0530

    Dansel Add Form

--- app/Views/Dansel/add.php
+++ app/Views/Dansel/add.php
@@ -141,26 +141,57 @@
                         </div>
 
                         <div class="form">
-                        <input class="search-field" type="text" name="lat" id="lat">
-                        <input class="search-field" type="text" name="lng" id="lng">
+                        <input class="search-field" type="hidden" name="lat" id="lat">
+                        <input class="search-field" type="hidden" name="lng" id="lng">
+                        </div>
+
+                        <div class="form">
+                            <h5>Opening Date</h5>
+                            <input data-role="date" type="text" placeholder="YYYY-MM-DD">
+                        </div>
+
+                        <div class="form">
+                            <div class="select">
+                                <h5>Poya Day</h5>
+                                <select data-placeholder="Choose Categories" class="chosen-select">
+                                    <option value="1">Duruthu Poya</option>
+                                    <option value="2">Navam Poya</option>
+                                    <option value="3">Medin Poya</option>
+                                    <option value="4">Bak Poya</option>
+                                    <option value="5">Vesak Poya</option>
+                                    <option value="6">Poson Poya</option>
+                                    <option value="7">Esala Poya</option>
+                                    <option value="8">Nikini Poya</option>
+                                    <option value="9">Binara Poya</option>
+                                    <option value="10">Vap Poya</option>
+                                    <option value="11">Il Poya</option>
+                                    <option value="12">Unduvap Poya</option>
+                                    <option value="13">Other</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form">
+                            <h5>Opening Time</h5>
+                            <input data-role="time" type="text" placeholder="HH:MM">
+                        </div>
+
+                        <div class="form">
+                            <h5>End Time</h5>
+                            <input data-role="time" type="text" placeholder="HH:MM">
                         </div>
 
                         <!-- Job Type -->
                         <div class="form">
-                            <h5>Job Type</h5>
-                            <select data-placeholder="Full-Time" class="chosen-select-no-single">
-                                <option value="1">Full-Time</option>
-                                <option value="2">Part-Time</option>
-                                <option value="2">Internship</option>
-                                <option value="2">Freelance</option>
-                            </select>
+                            <h5>Time</h5>
+                            <input data-role="time" type="text" placeholder="">
                         </div>
 
                         <!-- Choose Category -->
                         <div class="form">
                             <div class="select">
                                 <h5>Category</h5>
-                                <select data-placeholder="Choose Categories" class="chosen-select" multiple>
+                                <select data-placeholder="Choose Categories" class="chosen-select">
                                     <option value="1">Web Developers</option>
                                     <option value="2">Mobile Developers</option>
                                     <option value="3">Designers & Creatives</option>
@@ -173,69 +204,31 @@
                             </div>
                         </div>
 
-                        <!-- Tags -->
-                        <div class="form">
-                            <h5>Job Tags <span>(optional)</span></h5>
-                            <input class="search-field" type="text" placeholder="e.g. PHP, Social Media, Management" value="" />
-                            <p class="note">Comma separate tags, such as required skills or technologies, for this job.</p>
-                        </div>
-
                         <!-- Description -->
                         <div class="form">
                             <h5>Description</h5>
                             <textarea class="WYSIWYG" name="summary" cols="40" rows="3" id="summary" spellcheck="true"></textarea>
                         </div>
 
-                        <!-- Application email/url -->
-                        <div class="form">
-                            <h5>Application email / URL</h5>
-                            <input type="text" placeholder="Enter an email address or website URL">
-                        </div>
-
-                        <!-- TClosing Date -->
-                        <div class="form">
-                            <h5>Closing Date <span>(optional)</span></h5>
-                            <input data-role="date" type="text" placeholder="yyyy-mm-dd">
-                            <p class="note">Deadline for new applicants.</p>
-                        </div>
-
                         <!-- Company Details -->
                         <div class="divider">
-                            <h3>Company Details</h3></div>
+                            <h3>Organizing Committee Details</h3></div>
 
                         <!-- Company Name -->
                         <div class="form">
-                            <h5>Company Name</h5>
-                            <input type="text" placeholder="Enter the name of the company">
+                            <h5>Name</h5>
+                            <input type="text" placeholder="Enter the name">
                         </div>
 
                         <!-- Website -->
                         <div class="form">
-                            <h5>Website <span>(optional)</span></h5>
-                            <input type="text" placeholder="http://">
-                        </div>
-
-                        <!-- Teagline -->
-                        <div class="form">
-                            <h5>Tagline <span>(optional)</span></h5>
-                            <input type="text" placeholder="Briefly describe your company">
-                        </div>
-
-                        <!-- Video -->
-                        <div class="form">
-                            <h5>Video <span>(optional)</span></h5>
-                            <input type="text" placeholder="A link to a video about your company">
-                        </div>
-
-                        <!-- Twitter -->
-                        <div class="form">
-                            <h5>Twitter Username <span>(optional)</span></h5>
-                            <input type="text" placeholder="@yourcompany">
+                            <h5>Contact Number</span></h5>
+                            <input type="text" placeholder="0XXXXXXXXX">
                         </div>
 
                         <!-- Logo -->
                         <div class="form">
-                            <h5>Logo <span>(optional)</span></h5>
+                            <h5>Banner <span>(optional)</span></h5>
                             <label class="upload-btn">
                                 <input type="file" multiple />
                                 <i class="fa fa-upload"></i> Browse
@@ -244,7 +237,7 @@
                         </div>
 
                         <div class="divider margin-top-0"></div>
-                        <a href="#" class="button big margin-top-5">Preview <i class="fa fa-arrow-circle-right"></i></a>
+                        <a href="#" class="button big margin-top-5">Add <i class="fa fa-arrow-circle-right"></i></a>
 
                     </div>
                 </div>
