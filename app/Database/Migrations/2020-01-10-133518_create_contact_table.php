2e37fee4a05d07ef31783017afc79f41f4dd453f
sudarakas <sudharakafb@gmail.com>
2020-01-10 14:06:32 +0530

    contactus and feedback migration added

---
+++ app/Database/Migrations/2020-01-10-133518_create_contact_table.php
@@ -0,0 +1,49 @@
+<?php namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreatePlaygroundTables extends Migration
+{
+    /**
+     * Database creation is both an art and a science: there are many best practices,
+     * but there are also aspects that are left up to develop opinions. This migration
+     * file offers a few different philosophies to table definitions - follow the
+     * format that fits your style.
+     *
+     */
+    public function up()
+    {
+        /**
+         * Heroes
+         *
+         * We include all three data fields (created_at, updated_at, deleted_at) so we
+         * can use both features of CodeIgniter\Model, $useTimestamps and $useSoftDeletes.
+         */
+        $fields = [
+            'name' => ['type' => 'varchar', 'constraint' => 255],
+            'email' => ['type' => 'varchar', 'constraint' => 255],
+            'message' => ['type' => 'text'],
+            'created_at' => ['type' => 'datetime', 'null' => true],
+            'updated_at' => ['type' => 'datetime', 'null' => true],
+        ];
+
+        // 'id' is a buzzword that indicates to addField() that this will be a primary key
+        // We could also have provided this in $fields, as we do below
+        $this->forge->addField('id');
+        $this->forge->addField($fields);
+        // Keys help optimize database performance; we'll add some for fields we are likely
+        // to search or filter by
+        $this->forge->addKey('name');
+        $this->forge->addKey('created_at');
+
+        // While not necessary, indexing against `deleted_at` is a good idea if your model
+        // is using soft deletes, since most SELECT statements will include `deleted_at`
+
+        $this->forge->createTable('contactus');
+
+    }
+    public function down()
+    {
+        $this->forge->dropTable('dansels');
+    }
+}
---
+++ app/Database/Migrations/2020-01-10-133519_create_feedback_table.php
@@ -0,0 +1,50 @@
+<?php namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreatePlaygroundTables extends Migration
+{
+    /**
+     * Database creation is both an art and a science: there are many best practices,
+     * but there are also aspects that are left up to develop opinions. This migration
+     * file offers a few different philosophies to table definitions - follow the
+     * format that fits your style.
+     *
+     */
+    public function up()
+    {
+        /**
+         * Heroes
+         *
+         * We include all three data fields (created_at, updated_at, deleted_at) so we
+         * can use both features of CodeIgniter\Model, $useTimestamps and $useSoftDeletes.
+         */
+        $fields = [
+            'dansel_id' => ['type' => 'int', 'constraint' => 9],
+            'name' => ['type' => 'varchar', 'constraint' => 100],
+            'email' => ['type' => 'varchar', 'constraint' => 100],
+            'comment' => ['type' => 'varchar', 'constraint' => 1000],
+            'created_at' => ['type' => 'datetime', 'null' => true],
+            'updated_at' => ['type' => 'datetime', 'null' => true],
+        ];
+
+        // 'id' is a buzzword that indicates to addField() that this will be a primary key
+        // We could also have provided this in $fields, as we do below
+        $this->forge->addField('id');
+        $this->forge->addField($fields);
+        // Keys help optimize database performance; we'll add some for fields we are likely
+        // to search or filter by
+        $this->forge->addKey('dansel_id');
+        $this->forge->addKey('created_at');
+
+        // While not necessary, indexing against `deleted_at` is a good idea if your model
+        // is using soft deletes, since most SELECT statements will include `deleted_at`
+
+        $this->forge->createTable('feedback');
+
+    }
+    public function down()
+    {
+        $this->forge->dropTable('feedback');
+    }
+}
